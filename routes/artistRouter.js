98c061bbefd09ac77fe36eed06c111aa92de2a30
Ermain <paulermain@gmail.com>
2018-07-24 14:34:27 -0400

    Second Commit: Implemented basic CRUD functionality to return all objects in  the database but there are still some issues connecting to the database. TODO: Resolve database connectivity issue and implemented a few more CRUD method to return specific object by id, name, etc

--- routes/artistRouter.js
+++ routes/artistRouter.js
@@ -1,24 +1,47 @@
 const express = require('express');
+const Artist = require('../models/artists.model');
+
 const artistRouter = express.Router();
 
-artistRouter.get('/api/artists', (req, res) => {
-    res.json([
-        {
-            id: 1,
-            name: 'Trivium',
-            genre: 'Heavy Metal'
-        },
-        {
-            id: 2,
-            name: 'Whitechapel',
-            genre: 'Death Metal'
-        },
-        {
-            id: 3,
-            name: 'Amon Amarth',
-            genre: 'Heavy Metal'
-        }
-    ]);
-});
+// artistRouter.get('/api/artists', (req, res) => {
+//     res.json([
+//         {
+//             id: 1,
+//             name: 'Trivium',
+//             genre: 'Heavy Metal'
+//         },
+//         {
+//             id: 2,
+//             name: 'Whitechapel',
+//             genre: 'Death Metal'
+//         },
+//         {
+//             id: 3,
+//             name: 'Amon Amarth',
+//             genre: 'Heavy Metal'
+//         }
+//     ]);
+// });
+
+artistRouter.route('/artists')
+            .post(function(req, res){
+                console.log('/artists');
+                var artist = new Artist(req.body);
+
+                artist.save();
+                res.status(201).send(artist);
+            })
+            .get(function(req, res){
+                console.log('GET /artists');
+                
+                Artist.find(function(err, artists){
+                    if(err){
+                        res.status(404).send(err);
+                        return;
+                    }
+
+                    console.log(artists);
+                });
+            });
 
 
